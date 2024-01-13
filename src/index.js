90576526c0fb727cb6e6cd36da0397828f5356ad
Anand kushwaha <anandkushwaha@Anands-Mac-mini.local>
2024-01-14 00:49:11 +0530

    message queues

--- src/index.js
+++ src/index.js
@@ -3,12 +3,13 @@ const bodyParser = require("body-parser");
 const app = express();
 const { PORT } = require("./config/serverConfig");
 
-const { sendBasicEmail } = require("./services/email-service");
 const setupJobs = require("./utils/job");
+const { createChannel } = require("./utils/messageQueue");
 const ticketController = require("./controllers/ticket-controller");
-const startServer = () => {
+const startServer = async () => {
   app.use(bodyParser.json());
   app.use(bodyParser.urlencoded({ extended: true }));
+  const channel = await createChannel();
   app.post("/api/v1/tickets", ticketController.create);
   app.listen(PORT, () => {
     console.log("Starting server on port", PORT);
